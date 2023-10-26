3ebfe0b9359b06ecfcee5d2d6bdb331a513b0fb7
Aleksx07 <134466359+Aleksx07@users.noreply.github.com>
2023-10-26 17:35:32 -0600

    Initial commit

---
+++ app/src/main/java/hn/uth/pm01restapi_alekspineda/MainActivity.java
@@ -0,0 +1,14 @@
+package hn.uth.pm01restapi_alekspineda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
