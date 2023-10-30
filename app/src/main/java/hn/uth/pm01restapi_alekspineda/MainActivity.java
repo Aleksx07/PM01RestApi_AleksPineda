20ff6b179ee02bd9037c63565b49d37489160051
Aleksx07 <134466359+Aleksx07@users.noreply.github.com>
2023-10-30 15:51:19 -0600

    Commit - clase 26/10/2023

--- app/src/main/java/hn/uth/pm01restapi_alekspineda/MainActivity.java
+++ app/src/main/java/hn/uth/pm01restapi_alekspineda/MainActivity.java
@@ -1,8 +1,16 @@
 package hn.uth.pm01restapi_alekspineda;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest jsonObjectRequest = new StringRequest(Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/posts", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Respuesta",response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Respuesta",error.toString());
+            }
+        });
+
+        //Envio de la peticion
+        requestQueue.add(jsonObjectRequest);
+
+
+
     }
 }
